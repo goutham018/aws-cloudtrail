bc2e76367059ed04575dc1eda0d1d7f489125bf3
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-19 15:41:53 +0530

    updated changes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -93,11 +93,3 @@ resource "aws_cloudtrail" "trail" {
     aws_cloudwatch_log_group.trail-for-demo
   ]
 }
-
-output "log_group_name" {
-  value = aws_cloudwatch_log_group.trail-for-demo.name
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.trail_bucket.bucket
-}
