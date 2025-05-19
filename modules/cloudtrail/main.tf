f313297b77d137113814537d9c3db18538168c0f
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-19 14:45:31 +0530

    updated changes

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -14,7 +14,6 @@ resource "aws_cloudtrail" "main" {
   include_global_service_events = true
   is_multi_region_trail        = true
   enable_log_file_validation   = true
-  is_logging                   = true
   cloud_watch_logs_group_arn   = "${aws_cloudwatch_log_group.trail.arn}:*"
   cloud_watch_logs_role_arn    = var.cloudtrail_role_arn
 }
