34a2071dfa81d26c1f65e9f2cb786cef242c158d
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-19 15:18:51 +0530

    updated changes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "s3" {
-    bucket  = "my-aws-bucket-273550"
+    bucket  = "my-random-bucket-for-terraform-273550"
     key     = "goutham/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
